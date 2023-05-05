ba18a84f14f68131d2fb2b1974a60d2cf1ec322f
Samuel López Saura <samuellopezsaura@gmail.com>
2023-05-05 22:20:58 +0200

    feat: add configuration options to report engine and small fixes

--- services/report_engines/docx_js_report_engine/engine/conf.js
+++ services/report_engines/docx_js_report_engine/engine/conf.js
@@ -1,6 +1,7 @@
 const conf = {
     tmpFolder: "/tmp",
-    reportTemplate: "./templates/sample_template.docx"
+    reportTemplate: "./templates/sample_template.docx",
+    binding_address: "tcp://127.0.0.1:31337"
 }
 
 export default conf;
